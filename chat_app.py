d511b859dbc9002e2133e25ff2b5ad9a4f1b5449
Aditya Gupta <legendytgaming46@gmail.com>
2025-12-29 15:31:19 +0530

    Update chat_app.py

--- chat_app.py
+++ chat_app.py
@@ -18,7 +18,7 @@ model = genai.GenerativeModel("gemini-2.5-flash")
 
 # Page config
 st.set_page_config(page_title="Gemini Chat", page_icon="🤖")
-st.title("🤖 Gemini Multi-Turn Chat")
+st.title("🤖 Gemini Intelligent Chatbot")
 
 # Initialize chat history
 if "chat" not in st.session_state:
@@ -57,7 +57,7 @@ if user_prompt:
             for char in assistant_reply:
                 typed_text += char
                 placeholder.markdown(typed_text)
-                time.sleep(0.0002)  # typing speed (adjust)
+                time.sleep(0.02)  # typing speed (adjust)
 
     st.session_state.messages.append(
         {"role": "assistant", "content": assistant_reply}
@@ -68,6 +68,7 @@ if st.sidebar.button("🔄 Reset Conversation"):
     st.session_state.chat = model.start_chat(history=[])
     st.session_state.messages = []
     st.experimental_rerun()
+
 
 
 
